6f30ab94aba7406421873ed9d8f47d49cc73b3e8
prasasaw <34060802+prasasaw@users.noreply.github.com>
2018-11-20 17:19:03 +0100

    added folder for fns

--- cloudFunctions/index.js
+++ cloudFunctions/index.js
@@ -2,5 +2,5 @@
 
 exports.syncProducer = (req, res) => {
   res.set('Content-Type', 'application/json');
-  res.status(200).send(JSON.stringify({ success: true, message: "profile sent to pubsub topic successfully!" }));
+  res.status(200).send(JSON.stringify({ success: true, message: "profile pushed to pubsub topic successfully!" }));
   };
---
+++ cloudFunctions/syncProducer/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+exports.syncProducer = (req, res) => {
+  res.set('Content-Type', 'application/json');
+  res.status(200).send(JSON.stringify({ success: true, message: "profile sent to pubsub topic successfully!" }));
+  };
