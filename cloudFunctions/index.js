19c72670d2bd83dd7c9cf943d477847e84667d07
prasasaw <34060802+prasasaw@users.noreply.github.com>
2018-11-20 16:28:04 +0100

    Update cloudFunctions/index.js

--- cloudFunctions/index.js
+++ cloudFunctions/index.js
@@ -1,18 +1,6 @@
 'use strict';
 
-const PubSub = require('@google-cloud/pubsub');
-const pubsub = new PubSub();
-const topic = pubsub.topic('syncCustomer');
-const publisher = topic.publisher();
-
 exports.syncProducer = (req, res) => {
   res.set('Content-Type', 'application/json');
-  let customer = req.query.customer || req.body.customer;
-  publisher.publish(Buffer.from(JSON.stringify(customer)), (err) => {
-    if (err) {
-      res.status(500).send(JSON.stringify({ success: false, error: err.message }));
-      return;
-    }
-    res.status(200).send(JSON.stringify({ success: true, message: "profile sent to pubsub topic" }));
-  });
-};
+  res.status(200).send(JSON.stringify({ success: true, message: "profile sent to pubsub topic" }));
+  };
